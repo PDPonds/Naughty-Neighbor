af5c74dcbc086af778bd79f378b937b36548d630
Po2s <pondandmeen@gmail.com>
2024-06-11 18:00:08 +0700

    import authentication

--- naughty-neighbor/Assets/Scripts/UI/MainMenuUIManager.cs
+++ naughty-neighbor/Assets/Scripts/UI/MainMenuUIManager.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUIManager : Singleton<MainMenuUIManager>
 {
-   
+    [SerializeField] Button loginWithGoogleButton;
+    [SerializeField] Button loginAsGuestButton;
+
 }
