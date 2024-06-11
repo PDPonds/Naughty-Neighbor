bbd52c4c35bc0d5eff29d60849103faa0385e8fc
Po2s <pondandmeen@gmail.com>
2024-06-11 22:48:35 +0700

    import variable form google sheet

--- naughty-neighbor/Assets/Scripts/Game/GameManager.cs
+++ naughty-neighbor/Assets/Scripts/Game/GameManager.cs
@@ -2,7 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameMode { SinglePlayer, MultiPlayer }
+public enum GameDifficulty { Easy, Normal, Hard }
+public enum GameState { RichPick, AuntNextDoor }
+
 public class GameManager : Singleton<GameManager>
 {
+    public GameMode Mode = GameMode.SinglePlayer;
+    public GameDifficulty Difficulty = GameDifficulty.Easy;
+    public GameState State = GameState.AuntNextDoor;
+
+    public GameData gameData = new GameData();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    #region Game Mode
+
+    public void SwitchGameMode(GameMode mode)
+    {
+        Mode = mode;
+        switch (mode)
+        {
+            case GameMode.SinglePlayer:
+                break;
+            case GameMode.MultiPlayer:
+                break;
+        }
+    }
+
+    public bool IsGameMode(GameMode mode)
+    {
+        return Mode == mode;
+    }
+
+    #endregion
+
+    #region Game Difficulty
+
+    public void SwitchGameDifficulty(GameDifficulty difficulty)
+    {
+        Difficulty = difficulty;
+        switch (difficulty)
+        {
+            case GameDifficulty.Easy:
+                break;
+            case GameDifficulty.Normal:
+                break;
+            case GameDifficulty.Hard:
+                break;
+        }
+    }
+
+    public bool IsGameDifficulty(GameDifficulty difficulty)
+    {
+        return Difficulty == difficulty;
+    }
+
+    #endregion
+
+    #region Game State
+
+    public void SwitchGameState(GameState state)
+    {
+        State = state;
+        switch (State)
+        {
+            case GameState.AuntNextDoor:
+                break;
+            case GameState.RichPick:
+                break;
+        }
+    }
+
+    public bool IsGameState(GameState state)
+    {
+        return State == state;
+    }
+
+    #endregion
 
 }
---
+++ naughty-neighbor/Assets/Scripts/System/SheetDownloader.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SheetDownloader : MonoBehaviour
+{
+    const string sheetId = "12KB8eV75NC_4-saBfeA8Sw4qZHBDGo1wKuXJjTUgj0Q";
+    const string url = "https://docs.google.com/spreadsheets/d/" + sheetId + "/export?format=csv";
+
+    private void Start()
+    {
+        StartCoroutine(DownloadData());
+    }
+
+    IEnumerator DownloadData()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
+                webRequest.result == UnityWebRequest.Result.ProtocolError ||
+                webRequest.result == UnityWebRequest.Result.DataProcessingError)
+            {
+                Debug.Log("Download Error : " + webRequest.error);
+            }
+            else
+            {
+                GameManager.Instance.gameData = ProcessData(webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    public GameData ProcessData(string cvsData)
+    {
+        char lineEnding = '\n';
+        string[] rows = cvsData.Split(lineEnding);
+        int dataStartRowIndex = 1;
+        GameData gameData = new GameData();
+        for (int i = dataStartRowIndex; i < rows.Length; i++)
+        {
+            string[] cells = rows[i].Split(',');
+            switch (i)
+            {
+                case 1:
+                    gameData.PlayerHP = ParseInt(cells[3]);
+                    break;
+                case 2:
+                    gameData.EnemyHP_Easy = ParseInt(cells[3]);
+                    gameData.EnemyMissedRate_Easy = ParseInt(cells[4]);
+                    gameData.EnemyTargetHPToHeal_Easy = ParseInt(cells[5]);
+                    gameData.EnemySmallShotRate_Easy = ParseInt(cells[6]);
+                    break;
+                case 3:
+                    gameData.EnemyHP_Normal = ParseInt(cells[3]);
+                    gameData.EnemyMissedRate_Normal = ParseInt(cells[4]);
+                    gameData.EnemyTargetHPToHeal_Normal = ParseInt(cells[5]);
+                    gameData.EnemySmallShotRate_Normal = ParseInt(cells[6]);
+                    break;
+                case 4:
+                    gameData.EnemyHP_Hard = ParseInt(cells[3]);
+                    gameData.EnemyMissedRate_Hard = ParseInt(cells[4]);
+                    gameData.EnemyTargetHPToHeal_Hard = ParseInt(cells[5]);
+                    gameData.EnemySmallShotRate_Hard = ParseInt(cells[6]);
+                    break;
+                case 5:
+                    gameData.NormalAttack = ParseInt(cells[2]);
+                    break;
+                case 6:
+                    gameData.SmallAttack = ParseInt(cells[2]);
+                    break;
+                case 7:
+                    gameData.PowerThrow = ParseInt(cells[2]);
+                    break;
+                case 8:
+                    gameData.DoubleAttackAmount = ParseInt(cells[1]);
+                    gameData.DoubleAttack = ParseInt(cells[2]);
+                    break;
+                case 9:
+                    gameData.Heal = ParseInt(cells[3]);
+                    break;
+                case 10:
+                    gameData.TimeToThink = ParseInt(cells[7]);
+                    break;
+                case 11:
+                    gameData.TimeToWarning = ParseInt(cells[7]);
+                    break;
+            }
+        }
+        return gameData;
+    }
+
+    int ParseInt(string s)
+    {
+        int result = -1;
+        if (!int.TryParse(s, System.Globalization.NumberStyles.Integer,
+            System.Globalization.CultureInfo.GetCultureInfo("en-US"), out result))
+        {
+            Debug.Log("Can't parse int , wrong text.");
+        }
+        return result;
+    }
+
+}
+
+[Serializable]
+public class GameData
+{
+    public int PlayerHP;
+
+    public int EnemyHP_Easy;
+    public int EnemyMissedRate_Easy;
+    public int EnemyTargetHPToHeal_Easy;
+    public int EnemySmallShotRate_Easy;
+
+    public int EnemyHP_Normal;
+    public int EnemyMissedRate_Normal;
+    public int EnemyTargetHPToHeal_Normal;
+    public int EnemySmallShotRate_Normal;
+
+    public int EnemyHP_Hard;
+    public int EnemyMissedRate_Hard;
+    public int EnemyTargetHPToHeal_Hard;
+    public int EnemySmallShotRate_Hard;
+
+    public int NormalAttack;
+    public int SmallAttack;
+
+    public int PowerThrow;
+    public int DoubleAttackAmount;
+    public int DoubleAttack;
+    public int Heal;
+
+    public int TimeToThink;
+    public int TimeToWarning;
+}
+
