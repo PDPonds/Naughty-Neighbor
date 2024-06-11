cd89a4b3f5a38a584198c7ef12f10a654423d100
Po2s <pondandmeen@gmail.com>
2024-06-12 00:15:51 +0700

    setup main menu button script

--- naughty-neighbor/Assets/Scripts/UI/MainMenuUIManager.cs
+++ naughty-neighbor/Assets/Scripts/UI/MainMenuUIManager.cs
@@ -31,6 +31,15 @@ public class MainMenuUIManager : Singleton<MainMenuUIManager>
     [SerializeField] GameObject selectModePage;
     [SerializeField] GameObject selectModeBorder;
     [SerializeField] Button closeSelectModeButton;
+    [SerializeField] Button selectSinglePlayerButton;
+    [SerializeField] Button selectMultiPlayerButton;
+    [Header("===== Select Difficulty Page =====")]
+    [SerializeField] GameObject selectDifficultyPage;
+    [SerializeField] GameObject selectDifficultyBorder;
+    [SerializeField] Button closeDifficultyButton;
+    [SerializeField] Button easyModeButton;
+    [SerializeField] Button normalModeButton;
+    [SerializeField] Button hardModeButton;
 
     LoginManager loginManager;
 
@@ -42,6 +51,12 @@ public class MainMenuUIManager : Singleton<MainMenuUIManager>
         startGameButton.onClick.AddListener(StartGame);
         closeHowToPlayerButton.onClick.AddListener(CloseHowToPlay);
         closeSelectModeButton.onClick.AddListener(CloseSelectMode);
+        closeDifficultyButton.onClick.AddListener(CloseSelectDifficulty);
+        selectSinglePlayerButton.onClick.AddListener(SelectSinglePlayer);
+        selectMultiPlayerButton.onClick.AddListener(SelectMultiPlayer);
+        easyModeButton.onClick.AddListener(SelectEasyMode);
+        normalModeButton.onClick.AddListener(SelectNormalMode);
+        hardModeButton.onClick.AddListener(SelectHardMode);
     }
 
     #region Login Page
@@ -82,6 +97,39 @@ public class MainMenuUIManager : Singleton<MainMenuUIManager>
     void CloseSelectMode()
     {
         selectModePage.SetActive(false);
+    }
+
+    void SelectSinglePlayer()
+    {
+        selectDifficultyPage.SetActive(true);
+    }
+
+    void SelectMultiPlayer()
+    {
+        GameManager.Instance.SwitchGameMode(GameMode.MultiPlayer);
+    }
+
+    void SelectEasyMode()
+    {
+        GameManager.Instance.SwitchGameMode(GameMode.SinglePlayer);
+        GameManager.Instance.SwitchGameDifficulty(GameDifficulty.Easy);
+    }
+
+    void SelectNormalMode()
+    {
+        GameManager.Instance.SwitchGameMode(GameMode.SinglePlayer);
+        GameManager.Instance.SwitchGameDifficulty(GameDifficulty.Normal);
+    }
+
+    void SelectHardMode()
+    {
+        GameManager.Instance.SwitchGameMode(GameMode.SinglePlayer);
+        GameManager.Instance.SwitchGameDifficulty(GameDifficulty.Hard);
+    }
+
+    void CloseSelectDifficulty()
+    {
+        selectDifficultyPage.SetActive(false);
     }
 
     #endregion
